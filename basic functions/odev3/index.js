955f4181c2595a7b4d1af3734dd60d6cba9d5cea
hamdidilekci <98960357+hamdidilekci@users.noreply.github.com>
2022-05-27 18:32:06 +0300

    create odev2 folder
    create server

---
+++ basic functions/odev3/index.js
@@ -0,0 +1,27 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h2>WELCOME TO INDEX PAGE</h2>");
+  } else if (url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h2>WELCOME TO ABOUT US PAGE</h2>");
+  } else if (url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h2>WELCOME TO CONTACT PAGE</h2>");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write("<h2>404 NOT FOUND</h2>");
+  }
+
+  res.end();
+});
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`Sunucu port ${port} de başlatıldı.`);
+});
